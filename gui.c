b24459e46fd84e0afcfa7ab478ad24225bf760c2
Nagy Attila Gabor <mrbig@sneaker.hu>
2009-07-09 02:38:50 +0200

    Subwindows in setup window

--- gui.c
+++ gui.c
@@ -234,9 +234,12 @@ void CreateSetupWindow()
     x = 100;
     y = 650;
     w = 600;
-    h = 270;
+    h = 310;
     x2 = x + w;
     y2 = y - h;
+
+    // Gray windows
+    XPWidgetID w1, w2, w3;
 
     setupWindowWidget = XPCreateWidget(x, y, x2, y2,
                                        1, // Visible
@@ -246,15 +249,20 @@ void CreateSetupWindow()
                                        xpWidgetClass_MainWindow);
     XPSetWidgetProperty(setupWindowWidget, xpProperty_MainWindowHasCloseBoxes, 1);
 
+    /* Upper subwindows */
+    w1 = XPCreateWidget(x+10, y-60, x2-10, y-80, 1,
+                        "", 0, setupWindowWidget, xpWidgetClass_SubWindow);
+    XPSetWidgetProperty(w1, xpProperty_SubWindowType, xpSubWindowStyle_SubWindow);
+    
     /* smoothing scrollbar */
     CreateScrollbar(setupWindowWidget, &smoothingScrollbar, &smoothingValueCaption,
             "Smoothing:",
-            x+10, y-40, x+350,
+            x+20, y-50, x+350,
             getSmoothing(),
             1, 100);
 
     /* Radio button to enable/disable debugging */
-    debugCheckbox = XPCreateWidget(x + 400, y-40, x + 415, y - 60,
+    debugCheckbox = XPCreateWidget(x + 400, y-50, x + 415, y - 70,
                                         1,
                                         "",
                                         0,
@@ -262,84 +270,94 @@ void CreateSetupWindow()
                                         xpWidgetClass_Button);
     XPSetWidgetProperty(debugCheckbox, xpProperty_ButtonType, xpRadioButton);
     XPSetWidgetProperty(debugCheckbox, xpProperty_ButtonBehavior, xpButtonBehaviorCheckBox);
-    XPCreateWidget(x+415, y-40, x2-10, y-60, 1, "show debug window", 0, setupWindowWidget, xpWidgetClass_Caption);
+    XPCreateWidget(x+415, y-50, x2-20, y-65, 1, "show debug window", 0, setupWindowWidget, xpWidgetClass_Caption);
 
+    /* yaw subwindow */
+    w2 = XPCreateWidget(x+10, y-90, x+295, y-200, 1,
+                        "", 0, setupWindowWidget, xpWidgetClass_SubWindow);
+    XPSetWidgetProperty(w2, xpProperty_SubWindowType, xpSubWindowStyle_SubWindow);
+    
     /* yaw deadzone scrollbar */
-    XPCreateWidget(x+10, y - 70, x + 80, y-90, 1, "Yaw translation setup", 0, setupWindowWidget, xpWidgetClass_Caption);
+    XPCreateWidget(x+15, y - 90, x + 80, y-110, 1, "Yaw translation setup", 0, setupWindowWidget, xpWidgetClass_Caption);
 
     trcfg = getTranslationCfg(DOF_YAW);
 
     CreateScrollbar(setupWindowWidget, &yawDeadzoneScrollbar, &yawDeadzoneValueCaption,
             "Deadzone:",
-            x+10, y-90, x+280,
+            x+20, y-115, x+280,
             trcfg.deadzone,
             0, 30);
 
     CreateScrollbar(setupWindowWidget, &yawResponseScrollbar, &yawResponseValueCaption,
             "Response:",
-            x+10, y-120, x+280,
+            x+20, y-145, x+280,
             trcfg.response,
             0, 80);
 
     CreateScrollbar(setupWindowWidget, &yawAmplificationScrollbar, &yawAmplificationValueCaption,
             "Amplification:",
-            x+10, y-150, x+280,
+            x+20, y-175, x+280,
             trcfg.amplification,
             0, 100);
 
+    /* pitch subwindow */
+    w3 = XPCreateWidget(x+305, y-90, x2-10, y-200, 1,
+                        "", 0, setupWindowWidget, xpWidgetClass_SubWindow);
+    XPSetWidgetProperty(w3, xpProperty_SubWindowType, xpSubWindowStyle_SubWindow);
+
     /* pitch deadzone scrollbar */
-    XPCreateWidget(x+300, y - 70, x + 380, y-90, 1, "Pitch translation setup", 0, setupWindowWidget, xpWidgetClass_Caption);
+    XPCreateWidget(x+310, y - 90, x + 380, y-110, 1, "Pitch translation setup", 0, setupWindowWidget, xpWidgetClass_Caption);
 
     trcfg = getTranslationCfg(DOF_PITCH);
 
     CreateScrollbar(setupWindowWidget, &pitchDeadzoneScrollbar, &pitchDeadzoneValueCaption,
             "Deadzone:",
-            x+310, y-90, x2-10,
+            x+315, y-115, x2-20,
             trcfg.deadzone,
             0, 30);
 
     CreateScrollbar(setupWindowWidget, &pitchResponseScrollbar, &pitchResponseValueCaption,
             "Response:",
-            x+310, y-120, x2-10,
+            x+315, y-145, x2-20,
             trcfg.response,
             0, 80);
 
     CreateScrollbar(setupWindowWidget, &pitchAmplificationScrollbar, &pitchAmplificationValueCaption,
             "Amplification:",
-            x+310, y-150, x2-10,
+            x+315, y-175, x2-20,
             trcfg.amplification,
             0, 100);
 
     /* Wiimote connected subwindows */
-    wiimoteConnected = XPCreateWidget(x+40, y-185, x2-40, y2+25, 0,
+    wiimoteConnected = XPCreateWidget(x+40, y-235, x2-40, y2+25, 0,
                                       "Wiimote is connected",
                                       0, setupWindowWidget, xpWidgetClass_SubWindow);
 
     XPSetWidgetProperty(wiimoteConnected, xpProperty_SubWindowType, xpSubWindowStyle_SubWindow);
     XPAddWidgetCallback(wiimoteConnected, XPUFixedLayout);
-    XPCreateWidget(x+230, y - 210, x2 - 230, y-230, 1,
+    XPCreateWidget(x+240, y - 250, x2 - 240, y-270, 1,
                    "Wiimote is connected",
                    0, wiimoteConnected, xpWidgetClass_Caption);
 
 
     /* Wiimote disconnected subwindow, with connect button */
-    wiimoteDisconnected = XPCreateWidget(x+40, y-170, x2-40, y2+10, 0,
+    wiimoteDisconnected = XPCreateWidget(x+40, y-210, x2-40, y2+10, 0,
                                       "Wiimote is currently not connected",
                                       0, setupWindowWidget, xpWidgetClass_SubWindow);
 
     XPSetWidgetProperty(wiimoteDisconnected, xpProperty_SubWindowType, xpSubWindowStyle_SubWindow);
     XPAddWidgetCallback(wiimoteDisconnected, XPUFixedLayout);
 
-    XPCreateWidget(x+215, y - 180, x2 - 230, y-193, 1,
+    XPCreateWidget(x+215, y - 220, x2 - 230, y-233, 1,
                    "Wiimote is currently not connected",
                    0, wiimoteDisconnected, xpWidgetClass_Caption);
-    XPCreateWidget(x+90, y - 200, x2 - 10, y-213, 1,
+    XPCreateWidget(x+90, y - 240, x2 - 10, y-253, 1,
                    "To connect the wiimote you should put it first in discoverable mode (press 1+2),",
                    0, wiimoteDisconnected, xpWidgetClass_Caption);
-    XPCreateWidget(x+228, y - 213, x2 - 235, y-226, 1,
+    XPCreateWidget(x+224, y - 253, x2 - 235, y-266, 1,
                    "then click the button below.",
                    0, wiimoteDisconnected, xpWidgetClass_Caption);
-    connectButton = XPCreateWidget(x + 220, y-235, x2 - 220, y - 252,
+    connectButton = XPCreateWidget(x + 220, y-275, x2 - 220, y - 292,
                                         1,
                                         "Connect to Wiimote",
                                         0,
