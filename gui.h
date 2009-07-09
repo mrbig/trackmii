1b90e7f8e321374a74c7860fd68391dbeca28258
Nagy Attila Gabor <mrbig@sneaker.hu>
2009-07-10 00:56:00 +0200

    Added GPL copyright notices

--- gui.h
+++ gui.h
@@ -1,8 +1,20 @@
-/* 
- * File:   gui.h
- * Author: mrbig
+/* Copyright (C) 2009 Nagy Attila Gabor <nagy.attila.gabor@gmail.com>
  *
- * Created on 2009. j�nius 12., 18:23
+ *     This file is part of TrackMii.
+ *
+ *  TrackMii is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  TrackMii is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
+ *
  */
 
 #ifndef _GUI_H
